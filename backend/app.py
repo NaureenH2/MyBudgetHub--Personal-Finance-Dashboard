f8349b8f8b878d751af499464b4b3f01b7918913
NaureenH2 <naureen.hossain@torontomu.ca>
2025-12-18 16:05:33 -0500

    Setup

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 @app.route("/")
 def home():
-    return "MyBudgetHub is running!"
+    return "MyBudgetHub backend running!"
 
 if __name__ == "__main__":
     app.run(debug=True)
