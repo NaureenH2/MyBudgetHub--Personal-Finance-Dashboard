0a946c1136f094aae4e5bc18d330f11b2dfe1d2d
NaureenH2 <naureen.hossain@torontomu.ca>
2025-12-22 22:09:24 -0500

    Pie chart added

--- frontend/app.js
+++ frontend/app.js
@@ -8,7 +8,28 @@ async function loadExpenses() {
   const response = await fetch("http://127.0.0.1:5000/expenses");
   const data = await response.json();
   console.log("Expenses:", data);
+  renderExpenseChart(data);
 }
 
 loadBudgets();
 loadExpenses();
+
+function renderExpenseChart(expenses) {
+  const totals = {};
+
+  expenses.forEach(e => {
+    totals[e.category] = (totals[e.category] || 0) + e.amount;
+  });
+
+  const ctx = document.getElementById("expenseChart").getContext("2d");
+
+  new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: Object.keys(totals),
+      datasets: [{
+        data: Object.values(totals)
+      }]
+    }
+  });
+}
